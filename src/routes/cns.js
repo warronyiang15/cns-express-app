86156b05cac7f721bee651b103445814e2ee6753
warronyiang15 <warronyiang15@gmail.com>
2023-05-26 22:31:02 +0800

    done cookie-based authentication

--- src/routes/cns.js
+++ src/routes/cns.js
@@ -7,31 +7,7 @@ const asyncMiddleware = fn => (req, res, next) => {
       .catch(next);
 };
 
-/*
-The 'Basic' HTTP Authentication
-def index():
-    f = open('code3a.conf', 'r')
-    conf = f.read()
-    if 'Basic' in conf:
-        try:
-            if request.headers.get('Authorization') == None:
-                return construct_response("No authenticated credentials", 401, "text/html")
-            Authorization = request.headers.get('Authorization')
-            basic_header, encoded_credentials = Authorization.split(' ')
-            if basic_header != 'Basic':
-                return construct_response('Invalid Authorization Headers', 401, 'text/html')
-            try:
-                credentials = base64.b64decode(encoded_credentials).decode('utf-8')
-                username, password = credentials.split(':')
-                if username != 'CNS-user' or password != 'CNS-password':
-                    return construct_response('Invalid credentials', 401, 'text/html')
-                return construct_response('Good to go!', 200, 'text/html')
-            except:
-                return construct_response('Invalid credentials', 401, 'text/html')
-        except:
-            return construct_response('Invalid request', 401, 'text/html')
-*/
-
+/* The 'BASIC' Authentication Scheme 
 router.get('/', asyncMiddleware(async (req, res) => {
     const auth = req.headers['authorization'] || req.headers['Authorization']
     if( !auth ){
@@ -53,28 +29,36 @@ router.get('/', asyncMiddleware(async (req, res) => {
     }
     return res.status(401).json({'message': 'bye'});
 }));
-module.exports = router;
+*/
+
 /* 
 Cookie-based authentication
 */
-/*
 router.post('/', asyncMiddleware(async (req, res) => {
     //set cookies
     if( !req.body.username || !req.body.password ){
         return res.status(401).json({'message': 'bye'});
     }
+    console.log(req.body)
     const cookie = {
-        'username': req.body.username,
-        'password': req.body.password,
+        username: req.body.username,
+        password: req.body.password,
     };
     res.cookie(cookie).status(200).json({'message': 'free cookies'});
 }));
 
 router.get('/', asyncMiddleware(async (req, res) => {
     // get cookie
-    if( req.cookie)
+    console.log(req.cookie)
+    if( !req.cookie.username || !req.cookie.password ){
+        return res.status(401).json({'message': 'bye'});
+    }
+    if( req.cookie.username === 'CNS-user' && req.cookie.password === 'CNS-password' ){
+        return res.status(200).json({'message': "HERE YOU GO BABY!"});
+    }
+    return res.status(401).json({'message': 'bye'});
 }));
-*/
+
 /*
 JWT-based authentication
 */
@@ -85,3 +69,5 @@ JWT-based authentication
 //router.get('/', asyncMiddleware(async (req, res) => {
     //verify cookies
 //}));
+
+module.exports = router;
