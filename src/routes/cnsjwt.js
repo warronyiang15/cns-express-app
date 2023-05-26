7b8460801321d7a7581c3f478723e826aeb497d0
warronyiang15 <warronyiang15@gmail.com>
2023-05-26 23:09:00 +0800

    done cookie-based authentication

--- src/routes/cnsjwt.js
+++ src/routes/cnsjwt.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const asyncMiddleware = fn => (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+};
+
 /*
 JWT-based authentication
 */
